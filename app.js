7907a3f59f79d027ece6d8d05857ffe6c5a78de9
Pavel Perov <pavelperov93@gmail.com>
2019-09-23 15:41:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -75,10 +75,10 @@
 
  (function () {
     setTimeout(
-        $(document).ready(function() {
+
    $('.status:contains("Завършено")').css('color', 'green');
   $('.status:contains("Очаква одобрение")').css('color', 'orange'); 
-});
+
      
       }, 3000);
   })()
