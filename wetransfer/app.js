674bcac729635d4754b63185efdae3e726c72cf9
Pavel Perov <pavelperov93@gmail.com>
2019-11-18 18:02:55 +0200

    Update app.js

--- wetransfer/app.js
+++ wetransfer/app.js
@@ -4,6 +4,7 @@ const numItemsToGenerate = 1;
     fetch(`https://source.unsplash.com/1600x900/?beach`).then((response) => {   
       
       document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?beach')";
+        console.log(url('https://source.unsplash.com/1600x900/?beach'))
     }) 
   })()
 
