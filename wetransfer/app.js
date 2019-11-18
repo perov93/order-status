e6a6d45fa913ff4c9b6605fee296b83a41cbb051
Pavel Perov <pavelperov93@gmail.com>
2019-11-18 17:47:06 +0200

    Update app.js

--- wetransfer/app.js
+++ wetransfer/app.js
@@ -36,10 +36,5 @@ function copyLink() {
 
  // document.getElementById('result').innerHTML = $('#linkTransfer').val();
 
-return fetch(`https://source.unsplash.com/1600x900/?beach`)
-    .then(response => response.json())
-    .then(result => {
-      return result.results;
-    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?beach')";
-    });
+
 
