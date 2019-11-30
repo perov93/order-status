21434eb9006f9d8537a5df0c8e9cb2b7a0c10a53
Pavel Perov <pavelperov93@gmail.com>
2019-11-30 16:52:18 +0200

    Update currencyApi.js

--- currencyApi.js
+++ currencyApi.js
@@ -3,9 +3,14 @@ const getSecondUser = async () => {
     const response = await fetch(url);
     let commits = await response.json();
   let arrRates = commits.rates;
+	
+let foo = prompt('Currency Converter | From Euro to ... | Type your money in EURO');
+
+
+
   let curr = 1;
   const one = 1;
-  //console.log(commits);
+  console.log(commits);
   console.log(`---------------------------------------------`);
   console.log(`Base: ${commits.base}, by Date; ${commits.date}`);
   console.log(`---------------------------------------------`);
@@ -13,7 +18,7 @@ const getSecondUser = async () => {
   for (let key in arrRates) {
         
    
-    console.log(`Code: ${key}, Price: ${arrRates[key]}`);
+    console.log(`Code: ${key}, Price: ${arrRates[key]} | Converted price: ${(arrRates[key] * foo).toFixed(2)} ${key}`);
      
   }
   
